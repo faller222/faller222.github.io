5a0f3587874263a69e51eff909b358891e8bf25a
Falucho <faller222@gmail.com>
2022-05-15 21:17:25 -0300

    Deploy  -  giro

--- src/components/section/home.vue
+++ src/components/section/home.vue
@@ -67,8 +67,8 @@ export default {
       //Alpha -180 - 180
       //Beta -180 - 180
       //Gamma -90 - 90
-      const x = (Math.abs(e.alpha) - 90 ) * 5
-      const y = (90 - e.gamma) * 5
+      const x = Math.abs(e.alpha) * 5
+      const y = Math.abs(e.gamma) * 5
 
       this.move(x, y)
     }.bind(this), true)
