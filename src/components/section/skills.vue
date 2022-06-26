000a4eccc1e7b728039475ae85137c91cdfa8b3e
Falucho <faller222@gmail.com>
2022-06-26 20:24:41 -0300

    Deploy  -  Skills

--- src/components/section/skills.vue
+++ src/components/section/skills.vue
@@ -11,13 +11,9 @@
 
 
     <div class="container">
-      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-        literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
-        College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-        going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum
-        comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+      <p>10+ years of experience in IT, passionate troubleshooter, handling wide range of technologies.
+        Most experience comes from a national telecom company, multinational companies like BASF and Perficient.
+        Working with on-premise and cloud architectures, microservices oriented, with German or American customers.</p>
 
       <div class="cards">
         <div class="card" v-for="g in skills">
