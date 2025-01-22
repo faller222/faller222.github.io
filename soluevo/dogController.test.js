f0ec3569f560fa10d43a2bdd1dffd432ec85d771
Germán Faller <german.faller@favordelivery.com>
2025-01-22 19:16:11 -0300

    soluevo

--- soluevo/dogController.test.js
+++ soluevo/dogController.test.js
@@ -12,7 +12,6 @@ describe('Dog API Endpoints', () => {
     });
 
     test('GET /dog/random-dog should return a random dog JSON', async () => {
-        // Mock de la API externa
         nock('https://dog.ceo')
             .get('/api/breeds/image/random')
             .reply(200, { status: 'success', message: 'https://example.com/dog.jpg' });
@@ -28,10 +27,20 @@ describe('Dog API Endpoints', () => {
 
 
     test('GET /dog/random-dog should handle API errors', async () => {
-        // Mock de la API externa con un error
         nock('https://dog.ceo')
             .get('/api/breeds/image/random')
             .reply(500, { error: 'Internal Server Error' });
+
+        const response = await request(app).get('/dog/random-dog');
+
+        expect(response.status).toBe(500);
+        expect(response.body).toEqual({ error: 'Error fetching the image' });
+    });
+
+    test('GET /dog/random-dog should handle no Success status', async () => {
+        nock('https://dog.ceo')
+            .get('/api/breeds/image/random')
+            .reply(200, { status: 'error', message: 'Error test.' });
 
         const response = await request(app).get('/dog/random-dog');
 
